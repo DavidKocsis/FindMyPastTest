ff9b0c782cb92ab186968d06c430b9c8b849be8a
davidkocsis <david.kocsis85@gmail.com>
2016-04-10 11:20:23 +0100

    created methods to test if a number is a prime number

---
+++ findMyPastTest/UnitTests/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using findMyPastTest;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        private GetPrimes getPrimes = new GetPrimes();
+
+        [Test]
+        public void GetNumbersBelowSquareRootTest()
+        {
+            int inputNumber = 25;
+            var result = getPrimes.GetNumbersBelowSquareRoot(inputNumber);
+            Assert.AreEqual(result.Count(),5);
+        }
+
+        [Test]
+        public void GetNumbersBelowSquareRootTestWhenSquareRootIsNotWholeNumber()
+        {
+            int inputNumber = 37;
+            var result = getPrimes.GetNumbersBelowSquareRoot(inputNumber);
+            Assert.AreEqual(result.Count(), 6);
+        }
+
+        [Test]
+        public void CheckNumberIsPrimeReturnsFalseWhenNumberIsNotPrime()
+        {
+            int numberToTest = 25;
+            var result = getPrimes.IsNumberPrime(numberToTest);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void CheckNumberIsPrimeReturnsFalseWhenNumberIsPrime()
+        {
+            int numberToTest = 37;
+            var result = getPrimes.IsNumberPrime(numberToTest);
+            Assert.IsFalse(result);
+        }
+
+    }
+}
---
+++ findMyPastTest/findMyPastTest/GetPrimes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findMyPastTest
+{
+    public class GetPrimes
+    {
+        public List<int> GetNumbersBelowSquareRoot(int number)
+        {
+            var squareroot = Math.Sqrt(number);
+            int count = 1;
+            var numbers = new List<int>();
+            
+            while (count <= squareroot)
+            {
+                numbers.Add(count);
+                count++;
+            }
+
+            return numbers;
+        }
+
+        public bool IsNumberPrime(int numberToTest)
+        {
+            var numbers = GetNumbersBelowSquareRoot(numberToTest);
+            foreach (var number in numbers)
+            {
+                var result = numberToTest/number;
+                if (result%1 == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
